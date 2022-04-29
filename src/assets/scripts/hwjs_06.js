fd3bd4244e47e45df52fbc00592cef1f91b952c3
Anton Kurilov <anton0kurilov@gmail.com>
2022-04-29 18:54:00 +0700

    refactor: remove unused let

--- src/assets/scripts/hwjs_06.js
+++ src/assets/scripts/hwjs_06.js
@@ -1,7 +1,6 @@
 'use strict';
 
-let result = [], 
-    getCourseTitles = document.querySelectorAll('#basic-level .course__level-body-item-name-title');
+let getCourseTitles = document.querySelectorAll('#basic-level .course__level-body-item-name-title');
 for (let i = 0; i < getCourseTitles.length; i++) {
     document.querySelectorAll('#basic-level .course__level-body-item-name-title')[i].textContent = getCourseTitles[i].textContent.toUpperCase();
 }
