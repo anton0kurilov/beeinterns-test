60d9c84914b67608951099fef7221bca17135155
Anton Kurilov <anton0kurilov@gmail.com>
2022-04-29 22:02:24 +0700

    feat: slice too long sutitles in pro course level

--- src/assets/scripts/hwjs_06.js
+++ src/assets/scripts/hwjs_06.js
@@ -1,6 +1,17 @@
 'use strict';
 
-let getCourseTitles = document.querySelectorAll('#basic-level .course__level-body-item-name-title');
-for (let i = 0; i < getCourseTitles.length; i++) {
-    document.querySelectorAll('#basic-level .course__level-body-item-name-title')[i].textContent = getCourseTitles[i].textContent.toUpperCase();
+// HW N5.1
+
+let getCourseTitles = document.querySelectorAll('#basic-level .course__level-body-item-name-title'); // извлечение массива HTML-элементов с классом .course__level-body-item-name-title внутри элемента #basic-level
+for (let i = 0; i < getCourseTitles.length; i++) { // запуск цикла с числом повторов, равному длине массива
+    document.querySelectorAll('#basic-level .course__level-body-item-name-title')[i].textContent = getCourseTitles[i].textContent.toUpperCase(); // присвоение каждому элементу с классом .course__level-body-item-name-title внутри элемента #basic-level переведённого в верхний регистр значения
+}
+
+// HW N5.2
+
+let getCourseSubTitles = document.querySelectorAll('#pro-level .course__level-body-item-name-subtitle'); // извлечение массива HTML-элементов с классом .course__level-body-item-name-subtitle внутри элемента #pro-level
+for (let j = 0; j < getCourseSubTitles.length; j++) { // запуск цикла с числом повторов, равному длине массива
+    if (getCourseSubTitles[j].textContent.length > 19) { // если число символов в текущем значении массива больше 19,...
+        document.querySelectorAll('#pro-level .course__level-body-item-name-subtitle')[j].textContent = getCourseSubTitles[j].textContent.slice(0, 20) + '...'; // ... то вернуть первые 20 символов исходной строки вместе с многоточием
+    }
 }
