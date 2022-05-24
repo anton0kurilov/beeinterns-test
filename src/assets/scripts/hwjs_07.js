f2eb1cd98a0881bfae84ef851ac7d07037d2a68f
Anton Kurilov <anton0kurilov@gmail.com>
2022-05-24 23:22:02 +0700

    feat: add script for aside submenu showing/hiding with transition

---
+++ src/assets/scripts/hwjs_07.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function menuAccordion(menuType, option) {
+	const elementMenuContent = document.getElementById('submenu__' + menuType);
+	const elementMenuHeader = document.getElementById('mainmenu__' + menuType + '-item');
+	const elementMenuArrow = document.getElementById('mainmenu__' + menuType + '-arrow');
+
+	elementMenuHeader.addEventListener('click', function(event) {
+		if (!elementMenuContent.classList.contains('active')) {
+			elementMenuContent.classList.add('active');
+			elementMenuArrow.style.transform = 'rotate(0deg)';
+		} else {
+			elementMenuContent.classList.remove('active');
+			elementMenuArrow.style.transform = 'rotate(180deg)';
+		}
+	})
+}
+
+menuAccordion('programs', true);
+menuAccordion('lections', true);
